1335ca91c19d413ee02835c3dc7023cd4c407b28
Mateusz Byczkowski <byczkowski38@gmail.com>
2016-05-31 12:26:37 +0200

    Gamepad Stm32f4
    Działający skalibrowany gamepad dla STM, niekompatybilny z STL 1.2.

---
+++ ProjektCocox/defines.h
@@ -0,0 +1,8 @@
+#ifndef TM_DEFINES_H
+#define TM_DEFINES_H
+
+/* Uncomment if you want to enable USB HS in FS mode */
+/* Used on STM32F429 Discovery board */
+/* By default, USB FS mode is used */
+//#define USE_USB_OTG_HS
+#endif
